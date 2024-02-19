0a41ccfdf47b12b6b06e12147c1d57dae58a5ae4
SimoneCerri <iosonosimone@hotmail.it>
2024-02-19 16:04:22 +0100

    add the fix for second numbers after the float point

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //Ask kilometers to client
 let kilometers = Number (prompt ("How many kilometers do you will travel ?"));
-console.log(kilometers);
+console.log("Passanger will travel for: " + kilometers + "km");
 
 //Ask age to client
-let age = Number (prompt("What's your age ?"));
-console.log(age);
+let age = Number (prompt("What's your age again? cit."));
+console.log("Passanger's age: " + age + " years old");
 
 //Price = 0.21 * km
 let price = kilometers * 0.21;
-console.log(price);
+console.log("Ticket full price: " + price + "\u20AC");
 
 //if age < 18 = 20 % discount
 //if age > 65 = 40 % discount
@@ -25,8 +25,10 @@ else
 {
     discount = price ;
 }
-console.log(discount);
+console.log("Passenger's discount is " + discount + "\u20AC" );
 
 //final_price in human - form(2 max numbers after float point)
-let final_price = price - discount ;
-console.log(final_price);
+let final_price = (price - discount).toFixed(2) ;
+console.log("Ticket final price will be: " + final_price + "\u20AC");
+alert("You will pay " + final_price + " \u20AC for your ticket");
+
