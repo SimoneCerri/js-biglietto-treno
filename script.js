44de75e6574425783684ddaaaec87d097b149c96
SimoneCerri <iosonosimone@hotmail.it>
2024-02-19 15:28:20 +0100

    add prompt request and add variables

---
+++ script.js
@@ -0,0 +1,15 @@
+//Ask kilometers to client
+let kilometers = Number (prompt ("How many kilometers do you will travel ?"));
+console.log(kilometers);
+
+//Ask age to client
+let age = Number (prompt("What's your age ?"));
+console.log(age);
+
+//Price = 0.21 * km
+let price = kilometers * 0.21;
+console.log(price);
+
+//if age < 18 = 20 % discount
+//if age > 65 = 40 % discount
+//final_price in human - form(2 max numbers after float point)
